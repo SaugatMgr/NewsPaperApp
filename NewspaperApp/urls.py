0e97e9e0ebd22e0b2e07edb33ccbefecf8e5e82a
SaugatMgr <saugatmagar433@gmail.com>
2023-05-02 14:18:39 +0545

    adding urls for articles app

--- NewspaperApp/urls.py
+++ NewspaperApp/urls.py
@@ -21,6 +21,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('articles', include('articles.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path("", include('pages.urls')),
 ]
