56b86530df7030f4db6d9cafece5b2d606af6e65
SaugatMgr <saugatmagar433@gmail.com>
2023-05-02 15:10:04 +0545

    changed article_detail to article_list

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,5 @@ from . views import ArticleDetailView
 from django.urls import path
 
 urlpatterns = [
-    path('', ArticleDetailView.as_view(), name="article_detail"),
+    path('', ArticleDetailView.as_view(), name="article_list"),
 ]
