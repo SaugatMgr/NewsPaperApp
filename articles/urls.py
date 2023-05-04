435a879b7d864ef7c0c62b986eb66aca40dd2f52
SaugatMgr <saugatmagar433@gmail.com>
2023-05-04 13:45:16 +0545

    added url for create page articles/new/

--- articles/urls.py
+++ articles/urls.py
@@ -1,4 +1,5 @@
 from . views import (
+    ArticleCreateView,
     ArticleDetailView,
     ArticleListView,
     ArticleUpdateView,
@@ -9,6 +10,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name="article_list"),
+    path('new/', ArticleCreateView.as_view(), name="article_create"),
     path('<int:pk>/', ArticleDetailView.as_view(), name="article_detail"),
     path('<int:pk>/edit/', ArticleUpdateView.as_view(), name="article_edit"),
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name="article_delete"),    
